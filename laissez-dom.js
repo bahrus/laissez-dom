66875cdfb5b266cafc89d6bc5dbd504b28675ba9
bahrus <anderson.bruce.b@gmail.com>
2019-05-26 17:49:43 -0400

    ...

--- laissez-dom.js
+++ laissez-dom.js
@@ -1,22 +1,46 @@
 import { define } from 'trans-render/define.js';
 export class LaissezDOM extends HTMLElement {
+    constructor() {
+        super(...arguments);
+        this._cloned = false;
+    }
     static get is() { return 'laissez-dom'; }
     connectedCallback() {
-        Object.assign(this.style, {
-            minHeight: '25px',
-            display: 'block'
-        });
+        // Object.assign(this.style, {
+        //     minHeight: '25px',
+        //     display: 'block'
+        // } as CSSStyleDeclaration);
         this.style.minHeight = '25px';
         const ioi = {
             root: this.parentElement,
             rootMargin: '0px',
             threshold: 0.1
         };
-        this._observer = new IntersectionObserver(this.callback, ioi);
+        this._observer = new IntersectionObserver(this.callback.bind(this), ioi);
         this._observer.observe(this);
     }
+    disconnectedCallback() {
+        this._observer.disconnect();
+    }
+    cloneTemplate() {
+        const templ = this.querySelector('template');
+        if (templ === null) {
+            setTimeout(() => this.cloneTemplate(), 50);
+            return;
+        }
+        this.appendChild(templ.content.cloneNode(true));
+        templ.remove();
+        //this._cloned = true;
+    }
     callback(entries, observer) {
+        if (!this._cloned) {
+            this._cloned = true;
+            window.requestAnimationFrame(() => {
+                this.cloneTemplate();
+            });
+        }
         console.log({
+            this: this,
             entries: entries,
             observer: observer
         });
--- laissez-dom.ts
+++ laissez-dom.ts
@@ -3,12 +3,13 @@ export class LaissezDOM extends HTMLElement{
     static get is(){return 'laissez-dom';}
 
     _observer!: IntersectionObserver;
+    _cloned = false;
     connectedCallback(){
-        Object.assign(this.style, {
-            minHeight: '25px',
-            display: 'block'
+        // Object.assign(this.style, {
+        //     minHeight: '25px',
+        //     display: 'block'
 
-        } as CSSStyleDeclaration);
+        // } as CSSStyleDeclaration);
         this.style.minHeight = '25px';
         const ioi : IntersectionObserverInit = {
             root: this.parentElement,
@@ -16,12 +17,34 @@ export class LaissezDOM extends HTMLElement{
             threshold: 0.1
 
         };
-        this._observer = new IntersectionObserver(this.callback, ioi);
+        this._observer = new IntersectionObserver(this.callback.bind(this), ioi);
         this._observer.observe(this);
     }
 
+    disconnectedCallback(){
+        this._observer.disconnect();
+    }
+    cloneTemplate(){
+        const templ = this.querySelector('template');
+        if(templ === null){
+            setTimeout(() => this.cloneTemplate(), 50);
+            return;
+        }
+        this.appendChild(templ.content.cloneNode(true));
+        templ.remove();
+        //this._cloned = true;
+    }
+
     callback(entries: any, observer: any){
+        if(!this._cloned){
+            this._cloned = true;
+            window.requestAnimationFrame(() =>{
+                this.cloneTemplate();
+            })
+        }
+
         console.log({
+            this: this,
             entries: entries,
             observer: observer
         })
