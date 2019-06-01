50e8d1751bfc5cfe1f9f70f06b6cf8479f995d31
bahrus <anderson.bruce.b@gmail.com>
2019-06-01 15:02:22 -0400

    ...

---
+++ demo/genHTML.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const dirS = fs.readFileSync('demo/directory.json').toString();
+const dir = JSON.parse(dirS);
+const html = generateNode(dir);
+fs.writeFileSync('demo/directory.html', html);
+function generateNode(node){
+    let returnStr = '';
+    if(!node) return returnStr;
+    node.forEach(child =>{
+        if(child.children){
+            returnStr += /* html */`<details>
+    <summary>${child.name}</summary>
+    <laissez-dom>
+        <template>
+            <section>
+                ${generateNode(child.children)}
+            </section>
+        </template>
+    </laissez-dom>
+</details>
+`;
+        }else{
+            returnStr += /* html */`<div>${child.name}</div>`
+        }
+
+        
+    })
+    return returnStr;
+}
